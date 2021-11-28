db1f1ae7345801006ceb2b02e2ecf8e78ca43cf3
KazztheCajun <aautincreations@gmail.com>
2021-11-28 16:19:53 -0600

    configuring database access

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const USERS = require("../models/users");
 const shortid = require('shortid');
 const MongoClient = require('mongodb').MongoClient;
-const mongo = new MongoClient(process.env.API_KEY,{ useNewUrlParser: true, useUnifiedTopology: true });
+const mongoServer = process.env.PASS
+const mongo = new MongoClient(`mongodb+srv://${mongoServer}`,{ useNewUrlParser: true, useUnifiedTopology: true });
 const passport = require('../middlewares/passport-config');
 
 mongo.connect( async (err) => // all calls to the mongo database have to be made within the connect block
